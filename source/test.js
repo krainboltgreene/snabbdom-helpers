0918c871ba88eb2f444571ac732f00c9a2220b90
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-06-05 01:50:21 -0700

    rewriting tests with tap

--- source/test.js
+++ source/test.js
@@ -1,184 +1,24 @@
-import {describe, describe as context, it} from "mocha"
-import {expect} from "chai"
-import * as src from "../src"
-import {img} from "../src"
-import {a} from "../src"
+/* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 
-describe("src/index.js", () => {
-  context("when asked for a void element", () => {
-    context("and called with inner", () => {
-      it("throws an error", () => {
-        expect(() => img({inner: "Test"})).to.throw("No content allowed in void element <img>")
-      })
-    })
-  })
+import {test} from "tap"
+import {a} from "./"
 
-  context("when asked for any element", () => {
-    context("and called with selector", () => {
-      it("returns an object with the given selector in the .sel property appended to the element", () => {
-        expect(a({selector: ".value"})).to.have.property("sel", "a.value")
-      })
-    })
+test(({same, end}) => {
+  same(a({selector: ".value"}), {sel: "a.value"})
+  end()
+})
 
-    context("and called with inner", () => {
-      it("returns an object with the given inner in the .text property", () => {
-        expect(a({inner: "test"})).to.have.property("text", "test")
-      })
-    })
+test(({same, end}) => {
+  same(a({selector: ".value"}), {text: "test"})
+  end()
+})
 
-    context("and called with aria", () => {
-      it("returns an object with the given aria on in the .data property", () => {
-        expect(a({aria: {id: "1"}})).to.have.deep.property("data.aria.id", "1")
-      })
-    })
-  })
+test(({same, end}) => {
+  same(a({inner: "test"}), {text: "test"})
+  end()
+})
 
-  it("contains all of the W3C defined elements", () => {
-    expect(Object.keys(src)).to.deep.equal([
-      "a",
-      "abbr",
-      "acronym",
-      "address",
-      "applet",
-      "area",
-      "article",
-      "aside",
-      "audio",
-      "b",
-      "base",
-      "basefont",
-      "bdi",
-      "bdo",
-      "bgsound",
-      "big",
-      "blink",
-      "blockquote",
-      "body",
-      "br",
-      "button",
-      "canvas",
-      "caption",
-      "center",
-      "cite",
-      "code",
-      "col",
-      "colgroup",
-      "command",
-      "content",
-      "data",
-      "datalist",
-      "dd",
-      "del",
-      "details",
-      "dfn",
-      "dialog",
-      "dir",
-      "div",
-      "dl",
-      "dt",
-      "element",
-      "em",
-      "embed",
-      "fieldset",
-      "figcaption",
-      "figure",
-      "font",
-      "footer",
-      "form",
-      "frame",
-      "frameset",
-      "h1",
-      "h2",
-      "h3",
-      "h4",
-      "h5",
-      "h6",
-      "head",
-      "header",
-      "hgroup",
-      "hr",
-      "html",
-      "i",
-      "iframe",
-      "image",
-      "img",
-      "input",
-      "ins",
-      "isindex",
-      "kbd",
-      "keygen",
-      "label",
-      "legend",
-      "li",
-      "link",
-      "listing",
-      "main",
-      "map",
-      "mark",
-      "marquee",
-      "menu",
-      "menuitem",
-      "meta",
-      "meter",
-      "multicol",
-      "nav",
-      "nobr",
-      "noembed",
-      "noframes",
-      "noscript",
-      "object",
-      "ol",
-      "optgroup",
-      "option",
-      "output",
-      "p",
-      "param",
-      "picture",
-      "plaintext",
-      "pre",
-      "progress",
-      "q",
-      "rp",
-      "rt",
-      "rtc",
-      "ruby",
-      "s",
-      "samp",
-      "script",
-      "section",
-      "select",
-      "shadow",
-      "small",
-      "source",
-      "spacer",
-      "span",
-      "strike",
-      "strong",
-      "style",
-      "sub",
-      "summary",
-      "sup",
-      "table",
-      "tbody",
-      "td",
-      "template",
-      "textarea",
-      "tfoot",
-      "th",
-      "thead",
-      "time",
-      "title",
-      "tr",
-      "track",
-      "tt",
-      "u",
-      "ul",
-      "var",
-      "video",
-      "wbr",
-      "xmp",
-      "default"
-      // Exports!
-    ])
-  })
+test(({same, end}) => {
+  same(a({aria: {id: "1"}}), {data: {aria: {id: "1"}}})
+  end()
 })
